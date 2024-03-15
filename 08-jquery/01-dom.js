449c7daa31673253048057b57f85d77da7ab9cf1
Israel Ayala <iayala@pdx.edu>
2024-03-15 14:38:30 -0700

    jquery

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,6 +1,7 @@
 // Exercise #1:
 // When the user clicks the 'copy' button, copy the user input to the output area
 
+/*
 // Option #1
 // Fetch JavaScript objects representing specific elements in the DOM
 let userInput1 = document.querySelector('#userInput1');
@@ -15,6 +16,40 @@ function handleClick(event) {
   console.log('click event', event);
   output1.textContent = userInput1.value;
 }
+$("document").ready(() => {
+  $("#copy").on("click", () => {
+    $("output1").text($("#userInput1").val());
+  });
+});
+
+$("document").ready(() => {
+  $("#userInput2").on("input", () => {
+    $("output2").text($("#userInput2").val());
+  });
+});
+*/
+
+$(document).ready(() => {
+  // Exercise #1:
+  // When the user clicks the 'copy' button, copy the user input to the output area
+  // Fetch JavaScript objects representing specific elements in the DOM
+  // Add an event listener on the target element
+  // Callback function to handle event
+  $("#copy").on("click", (event) => {
+    console.log("Click event", event);
+    $("#output1").text($("#userInput1").val());
+  });
+
+  // Exercise #2:
+  // When the user enters input text, copy the user input to the output area
+  // Fetch JavaScript objects representing specific elements in the DOM
+  // Add an event listener on the target element
+  // Callback function to handle event
+  $("#userInput2").on("input", (event) => {
+    console.log("Input event", event);
+    $("#output2").text($("#userInput2").val());
+  });
+});
 
 // Option #2
 // document.getElementById('copy').onclick = () => {
@@ -35,14 +70,14 @@ function handleClick(event) {
 // When the user enters input text, copy the user input to the output area
 
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector('#userInput2');
-let output2 = document.querySelector('#output2');
+let userInput2 = document.querySelector("#userInput2");
+let output2 = document.querySelector("#output2");
 
 // Add an event listener on the target element
-userInput2.addEventListener('input', handleInput);
+userInput2.addEventListener("input", handleInput);
 
 // Callback function to handle event
 function handleInput(event) {
-  console.log('click event', event);
+  console.log("click event", event);
   output2.textContent = userInput2.value;
 }
--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,5 +1,61 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+$(document).ready(() => {
+  const url = "https://anapioficeandfire.com/api/books/";
 
+  const addBookToDOM = (item) => {
+    $("#books").append(
+      $("<div>")
+        .css({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        })
+        .append($("<h3>").text(item.name))
+        .append($("<p>").text(item.authors[0]))
+        .append($("<p>").text(item.released.substr(0, 4)))
+        .append($("<p>").text(`${item.numberOfPages} pages`))
+      /*
+
+        element.style.display = 'flex';
+        element.style.flexDirection = 'column';
+        element.style.alignItems = 'center';
+        element.style.marginTop = '20px';
+
+        title.textContent = item.name;
+        author.textContent = `by ${item.authors[0]}`;
+        published.textContent = item.released.substr(0, 4);
+        pages.textContent = `${item.numberOfPages} pages`;
+
+        */
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        console.log(data);
+
+        data.forEach((item) => {
+          addBookToDOM(item);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        $("#books")
+          .append("<div>")
+          .text(`An error occurred. Please try again.`);
+      },
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
+});
+/*
 const app = document.querySelector('#books');
 app.style.paddingLeft = 0;
 const loading = document.querySelector('#loading');
@@ -48,5 +104,5 @@ const fetchData = (url) => {
       app.removeChild(loading);
     });
 };
-
 fetchData(url);
+*/
